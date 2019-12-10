777efb5dd6fd0793407195e1e6e15f708401cc75
Ryan <cordingley.ryan@gmail.com>
2019-12-10 00:37:27 +0000

    completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: 300000)
+        Store.create(name: "Richmond", annual_revenue: 1260000)
+        Store.create(name: "Gastown", annual_revenue: 190000)
